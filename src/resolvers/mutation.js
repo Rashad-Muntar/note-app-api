044a0aa3450da0cf6dfa0665cf47427651f5d650
Rashad Muntar <muntartoure@gmail.com>
2022-09-20 17:32:01 +0000

    implmented authentication

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,9 @@
+const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
+const jwt  = require("jsonwebtoken");
+require("dotenv").config();
+const { AuthenticationError, ForbiddenError } = require("apollo-server-express");
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -19,21 +25,53 @@ module.exports = {
 
   updateNote: async (parent, { id, content }, { models }) => {
     try {
-     return await models.Note.findOneAndUpdate(
+      return await models.Note.findOneAndUpdate(
         {
           _id: id,
         },
         {
           $set: {
-            content
+            content,
           },
         },
         {
-          new: true
+          new: true,
         }
       );
     } catch (err) {
       return false;
     }
   },
+
+  signUp: async (parent, { username, email, password }, { models }) => {
+    email = email.trim().toLowerCase();
+    const avatar = gravatar.url(email);
+    const hashed = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        password: hashed,
+        avatar,
+      });
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRETE);
+    } catch (err) {
+      throw new Error("Opps there is something wrong in creating the account");
+    }
+  },
+
+  signIn: async(parent, {username, email, password}, {models}) => {
+    email = email.trim().toLowerCase()
+
+   
+        const user = await models.User.findOne({
+          $or: [{email}, {username}]
+        })
+        if(!user) throw new AuthenticationError("No user is found")
+        const valid = bcrypt.compare(password, user.password)
+        if(!valid) throw new AuthenticationError("Password does not match")
+        return jwt.sign({user: user._id}, process.env.JWT_SECRETE)
+   
+  }
 };
--- src/schema.js
+++ src/schema.js
@@ -1,26 +1,35 @@
-const {gql} =  require("apollo-server-express")
+const { gql } = require("apollo-server-express");
 
 module.exports = gql`
+  scalar DateTime
 
-scalar DateTime
-
-type Note {
+  type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
     createdAt: DateTime!
     updatedAt: DateTime!
   }
-  
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
+  }
+
   type Query {
     notes: [Note]
     note(id: ID!): Note!
-   
   }
 
   type Mutation {
     newNote(content: String!): Note!
     deleteNote(id: ID!): Boolean
     updateNote(id: ID!, content: String!): Note!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String!, email: String!, password: String!): String!
   }
+  
 `;
