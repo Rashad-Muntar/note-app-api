def9a4f27e9f03dba7d9138060545687ca887f31
Rashad Muntar <muntartoure@gmail.com>
2022-09-20 15:17:00 +0000

    add crud functionality of notes

---
+++ src/schema.js
@@ -0,0 +1,26 @@
+const {gql} =  require("apollo-server-express")
+
+module.exports = gql`
+
+scalar DateTime
+
+type Note {
+    id: ID!
+    content: String!
+    author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+  
+  type Query {
+    notes: [Note]
+    note(id: ID!): Note!
+   
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+    deleteNote(id: ID!): Boolean
+    updateNote(id: ID!, content: String!): Note!
+  }
+`;
